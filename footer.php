5020a6a2c9f33ac84e3cc18888a4a86b5484c9b8
PopzeLife <quentin.f.60@gmail.com>
2016-05-25 17:55:22 +0200

    Change footer to fit the updated forked version
    It now links to this fork.

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
 	    <?php if(isset($_GET['album'])) { ?>
 	    <p><?php printRSSLink('Album','','feed',' - subscribe to the album ' . getAlbumTitle(),true) ?></p>
 	    <?php } // endif ?>
-	    <p class="copyright">Copyright &copy; <?php echo date('Y'); ?> - All rights reserved.&nbsp; | &nbsp;Powered by <a href="http://zenphoto.org" title="ZenPhoto">ZenPhoto</a> &amp; <a href="https://github.com/amereservant/FlickerdTheme/" title="Flickerd Theme">Flickerd</a> theme</p>
+	    <p class="copyright">Copyright &copy; <?php echo date('Y'); ?> - All rights reserved.&nbsp; | &nbsp;Powered by <a href="http://zenphoto.org" title="ZenPhoto">ZenPhoto</a> &amp; <a href="https://github.com/Kant1-0/FlickerdTheme" title="Flickerd Theme (Fork)">Flickerd</a> theme v1.1</p>
 	</footer>
 	<?php zp_apply_filter('theme_body_close'); ?>
 </body>
